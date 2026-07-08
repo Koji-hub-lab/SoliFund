f7511bd0288f80a08032e019607a91e24b51a57f
koji_hub_lab <urichyamdjieu@gmail.com>
2026-07-08 22:44:53 +0100

    feat: initialisation du backend Spring Boot

---
+++ src/main/java/com/example/solidfund/SolidfundApplication.java
@@ -0,0 +1,13 @@
+package com.example.solidfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidfundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidfundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/solidfund/SolidfundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solidfund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidfundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
